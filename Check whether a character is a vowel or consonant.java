dd729133cbeb2886d8ee3125e4d1ee34361ab649
Sakshi43-dot <kadamsakshi43@gmail.com>
2025-07-31 20:41:20 +0530

    Add files via upload

---
+++ Check whether a character is a vowel or consonant.java
@@ -0,0 +1,15 @@
+public class Main 
+{
+    public static void main(String [] args)   
+    {
+        char c = 'Z';
+        if(c=='a'|| c=='e'|| c=='i'|| c=='o'|| c=='u'||c=='A'|| c=='E'|| c=='I'|| c=='O'|| c=='U')
+        {
+            System.out.println("It is vowel: " + c);
+        }
+        else
+        {
+            System.out.println("It is consonant: " + c);
+        }
+    }
+}
