8ec916e9fcfacc72036d1a9ef4ffc3efa0a3bdff
Sakshi43-dot <kadamsakshi43@gmail.com>
2025-07-31 20:31:25 +0530

    Add files via upload

---
+++ To find length of string.java
@@ -0,0 +1,14 @@
+public class Main
+{
+	public static void main(String[] args)
+    {
+    String s="HELLO PEOPLE";
+    int length=0;
+    for(int i=0;i<s.length();i++)
+    {
+    length++;
+    }
+    System.out.print(length);
+    }
+}
+        
