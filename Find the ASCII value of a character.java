2dc191e11e966301fb7fc9c8c1b32f9d38694549
Sakshi43-dot <kadamsakshi43@gmail.com>
2025-07-31 20:39:24 +0530

    Add files via upload

---
+++ Find the ASCII value of a character.java
@@ -0,0 +1,16 @@
+public class Main 
+{
+    public static void main(String [] args)   
+    {
+        
+		char c='A';	
+		
+		//typecasting from character type to integer type
+		int i = c;
+		
+		//printing ASCII value of the character
+		System.out.println("ASCII value of "+c+" is "+i);
+		
+		
+	}
+}    
